da73e326cce6b7551712e291ff344e89d79391a6
Shamanta Rezwana <srezwana4@gmail.com>
2024-03-29 11:48:39 +0600

    UI upgrated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,17 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { RecipeSearchComponent } from './recipe-search/recipe-search.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 export const routes: Routes = [
-    { path: "",component: HomeComponent},
+    // { path: "",component: HomeComponent},
+    { path: "",redirectTo: "login",pathMatch: 'full'},
     { path: "login",component: LoginComponent},
     { path: "signup",component: SignupComponent},
     { path: "addrecipe",component: AddRecipeComponent},
+    { path: "search",component: RecipeSearchComponent},
+    { path: "recipelist",component: RecipeListComponent},
+
+
 ];
